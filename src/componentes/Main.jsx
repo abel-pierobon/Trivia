5b6809c604636a889516fae6d7e2288efbe13cfe
nestorabel.pierobon <nestorabel.pierobon@gmail.com>
2024-10-09 13:37:05 -0300

    arreglos

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -5,8 +5,8 @@ import ModalTrivia from './ModalTrivia';
 function Main() {
     const [activar, setActivar] = useState(false);
     return (
-        <div className='flex flex-col justify-center mt-6 '>
-            <h1 className='text-3xl font-bold text-center mb-10'>Trivia Manual del Buen Conductor</h1>
+        <div className='flex flex-col justify-center mt-16 '>
+            <h1 className='text-3xl font-bold text-center mb-10 font-mono text-shadow'>Trivia Manual del Buen Conductor</h1>
             <button onClick={() => setActivar(!activar)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl flex justify-center mx-auto'> {activar ? 'Finalizar Trivia' : 'Activar Trivia'}</button>
             {activar && <ModalTrivia />}
         </div>
